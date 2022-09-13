b3edb14613dca5cb130352d46545d6dbd080aee8
nozwock <57829219+nozwock@users.noreply.github.com>
2022-09-13 21:23:08 +0530

    feat: Completed the new() method impl for Database

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
+use colored::*;
 use std::collections::HashMap;
 use std::fs::read_to_string;
+
+const DB_NAME: &str = "kvstore.db";
+const KVSTORE_DELIMITER: char = '=';
 
 fn main() {
     const valid_cmds: (&str, &str) = ("set", "get");
 
+    // accessing the args
     let mut cli_args = std::env::args().skip(1);
     let key = cli_args.next().unwrap();
     let value = cli_args.next().unwrap();
 
     println!("{:?}\n{:?}", cli_args, key);
 
-    let content = format!("{}={}\n", key, value);
+    // intializing the database
+    let kvstore = Database::new(DB_NAME.to_owned()).unwrap();
+
+    // chunk to write (set case)
+    let chunk = format!("{}{KVSTORE_DELIMITER}{}\n", key, value);
 }
 
 struct Database {
@@ -19,12 +28,23 @@ struct Database {
 }
 
 impl Database {
-    fn new(db_path: String) -> Database {
-        Database {
+    fn new(db: String) -> Result<Database, std::io::Error> {
+        // new(db: String) fn returns a Database instance populated with
+        // the entries from the database file.
+        let mut kvstore = Database {
             map: HashMap::new(),
-            db: db_path,
+            db,
         };
-        let contents = read_to_string(db_path).unwrap();
+        let content = read_to_string(&kvstore.db)?;
+        for line in content.lines() {
+            let (key, value) = line.split_once(KVSTORE_DELIMITER).unwrap();
+            // populating the database
+            kvstore
+                .map
+                .insert(key.to_owned(), value.to_owned())
+                .unwrap();
+        }
+        Ok(kvstore)
     }
     fn write() {}
     fn read() {}
